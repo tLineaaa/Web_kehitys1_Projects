964f12c9f940fd1414a823ad2eafddce6ac86a9b
Taru Laine <taru.s.laine@student.laurea.fi>
2026-04-24 15:32:50 +0300

    PROJEKTI2 jatkuu, missing img works yay

--- Projekti2/styles/javascript/toimintaa.js
+++ Projekti2/styles/javascript/toimintaa.js
@@ -14,7 +14,7 @@ function haeAlbumit(artisti) {
     fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${encodeURIComponent(artisti)}&api_key=${apiKey}&format=json`) //encodeURIComponent hyväksyy välilyönnit ja erikoismerkit
         .then ((response) => response.json())
         .then (function(data) {
-            console.log(data)
+            console.log(`Koko APIn vastaus: ${data}`);
             albumitPaikka.innerHTML = ""; //tyhjennä aiemmat albumit näkyviltä
 
 
@@ -23,25 +23,40 @@ function haeAlbumit(artisti) {
             const albumDiv = document.createElement("div"); // jokaiselle albumille oma kolo, class name albumDiv, esim. cssää varten
             albumDiv.className = "albumDiv";
 
-            
+
             const etsiKuva = album.image.find(function(i) { // etsitään albumin kuvaa
                 return i.size === "large"; // halutaan iso kuva, tarjolla kolmea kokoa
             });
 
             let aKuva;
-            if (etsiKuva) {
+            if (etsiKuva && etsiKuva["#text"] !== "") {
                 aKuva = etsiKuva["#text"];
             
-            // TÄÄLTÄ JATKUU - jos rikki tai mikä tulee console logiin, jos ei kuvaa / kun ei toimi
-            // Nyt drop down -valikko katoaa valinnan jälkeen - katso innerHTML vs appendchild!
-            
             } else {
-                aKuva = "../../images/eisaatavilla2.jpg"; // jos kuvaa ei saatavilla, näytä tämä kuva - toimiiko? entä risan kuvan kanssa?
+                aKuva = "images/eisaatavilla2.jpg"; // jos kuvaa ei saatavilla, näytä tämä kuva - toimiiko? entä risan kuvan kanssa?
             }
 
-            const nimiPaikka = document.querySelector("#nimiPaikka"); // Paikka artistin nimelle
-            nimiPaikka.innerHTML = `<h1>${album.artist.name}</h1>` // Artistin nimi näkyviin kaikkien albumien ylle
-            albumDiv.innerHTML = `<img src="${aKuva}" alt="${album.name}"><h3> ${album.name}</h3>`; // albumDiviin haettu kuva ja albumin nimi
+            const kuva = document.createElement("img");
+            kuva.src = aKuva;
+            kuva.alt = album.name
+
+            kuva.onerror = function() {
+                this.onerror = null;
+                this.src = "images/eisaatavilla2.jpg"
+            };
+
+// "Selain katsoo sitä vähän kuin yrittäisit asentaa renkaat kahvikuppiin."
+// "JavaScript on joskus kuin innokas siivooja: jos annat sille väärän huoneen avaimet, se tyhjentää koko talon."
+// "Nyt koodisi pitäisi taas herätä henkiin ilman dramatiikkaa. JavaScript rakastaa tällaisia kirjoitusvirheitä – ne ovat sen lempivälipalaa."
+
+//            const nimiPaikka = document.querySelector("#nimiPaikka"); // Paikka artistin nimelle
+//          nimiPaikka.innerHTML = `<h1>${album.artist.name}</h1>` // Artistin nimi näkyviin kaikkien albumien ylle
+//            albumDiv.innerHTML = `<img src="${aKuva}" alt="${album.name}"><h3> ${album.name}</h3>`; // albumDiviin haettu kuva ja albumin nimi
+
+            albumDiv.appendChild(kuva); // lisää kuva albumDiviin
+            const albumiNimi = document.createElement("h3"); // määritellään documenttiin lisättäväksi h3
+            albumiNimi.textContent = album.name; // sisältö on albumin nimi
+            albumDiv.appendChild(albumiNimi); //lisää albumin nimi
 
             // Kesken, klikatessa saataisiin kappaleet
             albumDiv.addEventListener("click", function() {
@@ -53,18 +68,43 @@ function haeAlbumit(artisti) {
         });
         }
 
-valinta.addEventListener('change', function() { // kuuntelija, joka reagoi siihen, kun vaihdetaan alasvetovalikon valintaa
-    if (valinta.value !== "Avaa valikko") { // kun arvo siinä on muu kuin "Avaa valikko",
-        haeAlbumit(valinta.value); // aja funktio haeAlbumit() valinta-arvon mukaan
-    }
-});
+if(valinta) {
+    valinta.addEventListener('change', function() { // kuuntelija, joka reagoi siihen, kun vaihdetaan alasvetovalikon valintaa
+        if (valinta.value !== "Avaa valikko") { // kun arvo siinä on muu kuin "Avaa valikko",
+            haeAlbumit(valinta.value); // aja funktio haeAlbumit() valinta-arvon mukaan
+        }
+});}
 
-etsi.addEventListener('input', function() { // kuuntelija, joka reagoi hakukentän syötteeseen
-    if (etsi.value !== "") { // kunhan ei ole tyhjä kentä,
-        haeAlbumit(etsi.value); // aja funktio haeAlbumi() etsi-arvon mukaan
-    }
-});
+if(etsi) {
+    etsi.addEventListener('input', function() { // kuuntelija, joka reagoi hakukentän syötteeseen
+        if (etsi.value !== "") { // kunhan ei ole tyhjä kentä,
+            haeAlbumit(etsi.value); // aja funktio haeAlbumi() etsi-arvon mukaan
+        }
+});}
 
 
 // let data = JSON.parse (xmlhttp.responseText)
 // console.log(data)
+
+// ullatus.html:n työstämistä JATKA SYÖTTEEN TULOSTEEN KANSSA
+
+const catApiKey = "live_Qq9TZXOog8U05SHE7mI2ELkWvNV4zC9D2p3DJYLqdRG7VqtsNKWQbHAoW8rljGfT";
+const ullatusPaikka = document.querySelector("#ullatusPaikka");
+const kissaNappi = document.querySelector("kissaNappi")
+
+function haeKisu() {
+    fetch = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${catApiKey}` // hae apikeyn kanssa kuvat (määärä 10, rotu beng) - MUOKKAA?
+    
+    .then((response) => response.json()) // palauta vastaus json muodossa
+
+    .then(data => console.log(`KisuApin tulos ${data}`)) // logitetaan konsoliin
+
+    .catch(error => console.error(`Virhe: ${error}`))
+};
+
+// PITIKÖ TEHDÄ TARKASTUS JSONIN KANSSA VAKIOTARKASTUS?
+
+if(kissaNappi) { // tarkistetaan, että kissanappi löytyy
+    kissaNappi.addEventListener('click', function() {
+    haeKisu(); 
+});}
