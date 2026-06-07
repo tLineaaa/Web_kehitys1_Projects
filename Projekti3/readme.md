58adec5052018483515a40f67d9c148193daaa6e
Taru Laine <taru.s.laine@student.laurea.fi>
2026-06-07 15:37:11 +0300

    jQuery UI, jQuery koodinlyhennystä, anime.js

--- Projekti3/readme.md
+++ Projekti3/readme.md
@@ -1,2 +1,125 @@
-Kolmosprojekti tänne
-Aiheena javascript-kirjastot
+<h1> JS-sovellus ulkoisia kirjastoja käyttäen </h1>  
+Web kehitys 1 (FRONT END)- kurssin kolmannen projektin työstöä  
+
+
+## Projektin nimi ja tekijät
+Projekti 3 Musiikki (ja kissa) API JS-kirjastoja hyödyntäen  
+Taru Laine  
+
+
+## Verkkolinkit:
+Pääset julkaistuun sovellukseen käsiksi osoitteessa: [Musiikki API](https://tlineaaa.github.io/Web_kehitys1_Projects/Projekti3/)  
+Projektin videoesittelyn url toimitettu palautuksen kommenttikentässä.  
+
+
+## Oma arvio työstä ja oman osaamisen kehittymisestä
+Projektini täyttää tehtävänannon vaatimukset:  
+jQueryä on käytetty AJAXin ja DOM-skriptauksen tekemiseen.
+Sovelluksen ulkoasussa on käytetty UI-kirjastoa (Bootstrap, jQuery UI + kevyet granim.js & anime.js)
+Koodi on julkaistu GitHubissa ja sovellus GitHubin kautta.
+Videodemo sekä projektiraportit on laadittu.
+
+Opin alkeet siitä, kuinka JavaScript-kirjastoja voidaan hyödyntää.  
+Hyödynsin näitä oppeja lisämäällä animoinnin sekä taustakuvaan,  
+että "Valitse artisti" -otsikkoon (h3).  
+Ekstrana lisäsin myös tausta-animaation pysäytyspanikkeen (käytettävyys huomioiden).  
+
+Harjoittelin ja opin myös jQueryn käyttöä, jota hyödynsin projektissa  
+lyhentääkseni koodia.  
+  
+Antaisin itselleni pisteitä 10/10 p.  
+
+
+## Palaute opettajalle kurssista sekä itse opetuksesta tähän saakka
+Kiitos, että annat aina tilaa kysymyksille ja keskustelulle.  
+Tämä projekti tuntui alkuun haastavalta ja aiheen parissa voisin  
+varmasti vielä jatkaa työskentelyä, jotta alkaa sujumaan sutjakkaammin.  
+
+
+## Sisällysluettelo:
+- [Tietoja sovelluksesta](#tietoja-sovelluksesta)
+- [Tunnetut virheet/bugit](#tunnetut-virheet-tai-bugit)
+- [Kuvakaappaukset](#kuvakaappaukset)
+- [Teknologiat](#teknologiat)
+- [Asennus](#asennus)
+- [Lähestymistapa](#lähestymistapa)
+- [Kiitokset](#kiitokset)
+- [Lisenssi](#lisenssi)
+
+
+## Tietoja sovelluksesta
+Sovellus on Musiikki API, jossa voit etsiä joko alasvetovalikosta
+tai hakukentään kirjoittaen haluamaasi artistia. Sivulle tulostuu
+näkymä haetun artistin albumeista kuvan ja tekstin kera.
+
+Ylläri-osiossa voit klikata nappia ja aloittaa juhlat.
+Myös tässä on hyödynnetty APIa, joskin kissa-apia.
+
+
+## Tunnetut virheet tai bugit
+API-avaimet ovat nyt näkyvillä, joten siitä GitHub voi herjata.
+Avaimet voi piilottaa ja sen tekisin seuraavan vastaavan projektin kohdalla.
+
+## Kuvakaappaukset
+
+**Etusivu: Valitse artisti alasvetovalikosta tai etsi hakukenttään kirjoittaen**  
+![Etusivu: Valitse artisti alasvetovalikosta tai etsi hakukenttään kirjoittaen ](images/index.jpg)  
+
+**Valittu artisti Kiraw + albumien tiedot kuvien kanssa** 
+![Kissa API -puolen iloinen kissa ](images/albumit.jpg)  
+
+**Kissa API -puolen iloinen kissa**  
+![Kissa API -puolen iloinen kissa ](images/ilokissa.jpg)  
+
+Kuvat: Taru Laine  
+
+
+## Teknologiat
+Projektissa on hyödynnetty niin HTMLää, CSSää kuin JavaScriptiä.
+Html:n navikointi- ja hakupalkin muotoilussa on hyödynnetty Bootstrapia.
+Muutoin muotoilu on luotu tyylit.css -tiedostoon.
+Tapahtumat on luotu DOM-skriptauksella toimintaa.js -tiedostoon.
+
+Pääpaino on JavaScriptin puolella, jossa suoritetaan AJAX-kutsu (fetch-kutsu) kahteen
+eri API-osoitteeseen. Tämän jälkeen luodaan halutuille tiedoille paikat ja
+näytetään tuodut tiedot.
+
+
+## Asennus
+Sovellus toimii suoraan github.io:ssa [Musiikki API](https://tlineaaa.github.io/Web_kehitys1_Projects/Projekti3/index.html)  
+
+Linkin avattua etusivulla on alasvetovalikko, jossa lukee "Avaa valikko".  
+Sitä klikkaamalla voi valikoida listalta artistin, jonka jälkeen valitun artistin  
+albumit tulevat näkyviin nimien sekä albumikuvien kera.  
+     
+Oikeassa yläkulmassa on hakukenttä, jonka vieresssä lukee "Etsi artistin mukaan".  
+Kun kohtaan "Kirjoita tähän..." alkaa kirjoittamaan haluamansa artistin tai bändin nimeä,   
+näkyy hakukentän alapuolella ehdotustulos ja ruudulla alkaa näkyä kirjoitetun artistin  albumeja.  
+  
+Bonuksensa voit klikata vasemman ylävalikon "Ylläri"-painiketta.  
+Nyt ruudulla näkyy "Ei saatavilla"-kuvasta tuttu surullinen kissa,  
+jonka alapuolella on "Party button". Jos klikkaat painiketta,  
+alkaa musiikki soida, surullinen kissa muuttuu iloiseksi ja  
+sen kaverit ilmestyvät tanssimaan.  
+Kun on aika lopettaa bileet, klikkaa "Stop"-painiketta.  
+
+
+## Kiitokset
+  
+Hyödynsin projektin teossa Laurean Web-kehitys 1 (front end)-kurssin kurssimateriaalia  
+sekä omaa aiempaa [API-projektiani](https://github.com/tLineaaa/Web_kehitys1_Projects/tree/f336b3953503515c4ca79e88e48c04e4bbad5526/Projekti2)  
+
+Käytin ChatGPT:tä debuggauksessa.  
+ 
+
+Hyödynsin myös vinkkejä ja keskustelujen kommentteja sivustoilta:  
+[Bootstrap](https://getbootstrap.com/)  
+[Animejs](https://animejs.com/documentation/getting-started/)  
+[Granim.js](https://sarcadass.github.io/granim.js/)  
+[Acharya, D. P. 2025. The Best JavaScript Libraries and Frameworks. Kinsta](https://kinsta.com/blog/javascript-libraries/#the-most-popular-javascript-libraries)  
+
+Musiikki: [Brazilian Phonk by The_Mountain](https://pixabay.com/music/edm-brazilian-phonk-505181/)
+
+
+## Lisenssi
+[MIT-lisenssi](../LICENSE) @ [Taru Laine](https://github.com/tLineaaa)  
--- Projekti3/styles/toimintaa.js
+++ Projekti3/styles/toimintaa.js
@@ -1,4 +1,4 @@
-// ! js-kirjasto
+
 // Taustakuvan elävöittäminen granim.js:n avulla https://sarcadass.github.io/granim.js/index.html
 var granimInstance = new Granim({
     element: '#canvas-image-blending',
@@ -17,7 +17,7 @@ var granimInstance = new Granim({
                 ['#80d3fe', '#7ea0c4'],
                 ['#f0ab51', '#eceba3']
             ],
-            transitionSpeed: 300
+            transitionSpeed: 330 //nopeus "musiikille" sopivaksi
         }
     }
 });
@@ -53,7 +53,7 @@ function haeAlbumit(artisti) {
                 aKuva = etsiKuva["#text"];
             
             } else {
-                aKuva = "images/eisaatavilla2.jpg"; // jos kuvaa ei saatavilla, näytä tämä kuva - toimiiko? entä risan kuvan kanssa?
+                aKuva = "media/eisaatavilla2.jpg"; // jos kuvaa ei saatavilla, näytä tämä kuva - toimiiko? entä risan kuvan kanssa?
                 aKuva.classList.add("varakuva");
             }
 
@@ -63,7 +63,7 @@ function haeAlbumit(artisti) {
 
             kuva.onerror = function() {
                 this.onerror = null;
-                this.src = "images/eisaatavilla2.jpg";
+                this.src = "media/eisaatavilla2.jpg";
                 this.classList.add("varakuva");
             };
 
@@ -73,16 +73,24 @@ function haeAlbumit(artisti) {
             albumDiv.appendChild(albumiNimi); //lisää albumin nimi
             
             albumitPaikka.appendChild(albumDiv);
-        });
-        });
-        }
+});
 
-if(valinta) {
-    valinta.addEventListener('change', function() { // kuuntelija, joka reagoi siihen, kun vaihdetaan alasvetovalikon valintaa
-        if (valinta.value !== "Avaa valikko") { // kun arvo siinä on muu kuin "Avaa valikko",
-            haeAlbumit(valinta.value); // aja funktio haeAlbumit() valinta-arvon mukaan
-        }
-});}
+        });
+}
+
+//valinta-valikon toiminta jQuerylla
+$("#valinta").change(function () {
+    if ($("#valinta").val() !== "Avaa valikko") { // ("#valinta") tilalla voisi olla myös this, joka viittaa viimeisimpään $
+        haeAlbumit($("#valinta").val()); // ja .val vähän sama kuin aiemmin value, mutta funktiona vrt. $("#asianNimi).text() tai sama, mutta loppu html()
+    }
+});
+
+// sama kuin yllä, mutta ilman jQuerya
+//if(valinta) {
+//    valinta.addEventListener('change', function() { // kuuntelija, joka reagoi siihen, kun vaihdetaan alasvetovalikon valintaa
+//        if (valinta.value !== "Avaa valikko") { // kun arvo siinä on muu kuin "Avaa valikko",
+//            haeAlbumit(valinta.value); // aja funktio haeAlbumit() valinta-arvon mukaan
+//        }});}
 
 if(etsi) {
     etsi.addEventListener('input', function() { // kuuntelija, joka reagoi hakukentän syötteeseen
@@ -95,5 +103,105 @@ if(etsi) {
 // jQueryn testaus
 $("#taustaSeis").click(function() {
     granimInstance.pause()
-
 })
+
+
+// animejs testaus
+const { animate, stagger, splitText } = anime; // nämä toiminnot animejs:stä
+const { chars } = splitText('h3', { words: false, chars: true, includeSpaces: true }); // määritellään kohde, säilytetään välilyönnit
+
+// animoidaan merkit (ei pelkät sanat, kts. yllä)
+animate(chars, {
+  // Property keyframes
+  y: [
+    { to: '-2.75rem', ease: 'outExpo', duration: 600 },
+    { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
+  ],
+  // Property specific parameters
+  rotate: {
+    from: '-1turn',
+    delay: 0
+  },
+  delay: stagger(50),
+  ease: 'inOutCirc',
+  loopDelay: 1000,
+  loop: false // toistaa vain kerran, jotta otsikko on luettavissa eikä rasittava
+});
+
+// kissabileet
+const catApiKey = "live_Qq9TZXOog8U05SHE7mI2ELkWvNV4zC9D2p3DJYLqdRG7VqtsNKWQbHAoW8rljGfT"; // apiKey
+const ullatusPaikka = document.querySelector("#ullatusPaikka"); // määritellään paikka yllätykselle
+const kissaNappi = document.querySelector("#kissaNappi") // valikoidaan nappi
+const musiikki = new Audio("media/the_mountain-brazilian-phonk-505181.mp3"); // luodaan audio (haetaan kansiosta sound)
+const tanssiTyyli = ["hyppy", "hyppy2", "hyppy3"]
+let i = 0;
+let sankariKissa = document.querySelector("#suru")
+const sankarinPaikka = document.querySelector("#juhlaKissa")
+
+function haeKisu() {
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=50&breed_ids=sava,beng,norw,mcoo,ragd&api_key=${catApiKey}`) // hae apikeyn kanssa kuvat (määrä 50, rotu sava, beng yms)
+    
+    .then((response) => response.json()) // palauta vastaus json muodossa
+
+    .then(data => {
+        console.log("KisuApin tulos", data); // logitetaan konsoliin
+        
+        ullatusPaikka.innerHTML = ""; // tyhjä paikka
+    
+        data.forEach(kissa => { // jokaista kissaa kohti
+        const uusiKissaKuva = document.createElement("img"); // luo kuva
+        uusiKissaKuva.src = kissa.url; // kuvan lähde on kissa.url (saatu fetchillä)
+        uusiKissaKuva.classList.add("kisu", tanssiTyyli[i % 3]); // lisätään class ja tanssityyli
+        i++;
+        ullatusPaikka.appendChild(uusiKissaKuva); // lisää kuva ullatusPaikkaan
+        });
+    })
+    .catch(error => console.error(`Virhe: ${error}`)); //näytä virhe console logissa
+}
+
+function vaihdaIloksi() { //Luodaan funktio
+    sankariKissa.src = "media/iloinenkisu.png"; // joka vaihtaa sankariKissan lähteeksi iloinenkisu.png:n
+}
+
+
+function lisaaStop() { // Luodaan funktio
+    let stopPaikka = document.querySelector("#stopPainike"); // paikka määritelty
+    stopPaikka.innerHTML = ""; // tyhjenentty em. paikka
+    let stop = document.createElement("button"); // luodaan nappi
+    stop.id = "stopNappi"; // annetaan sille id
+    stop.textContent = "Stop"; // sen tekstisisältö on Stop
+    stopPaikka.appendChild(stop); // lisätään painike paikoilleen
+
+    if(stop) { // Jos stop-nappi löytyy
+        stop.addEventListener("mouseenter", function() { // kun hiiri menee sen ylle
+            stop.style.backgroundColor="#c50101"; // vaihdetaan väri tummemmaksi
+        })
+
+        stop.addEventListener("mouseleave", function() { // kun hiiri lähtee napin yltä
+            stop.style.backgroundColor="#ff0000"; // väri vaihtuu takaisin alkuperäiseksi
+        })
+    }
+
+    stop.addEventListener('click', function() { // Lisätään kuuntelija stop-napille, klikatessa
+        musiikki.pause();  // musiikki paussille
+        granimInstance.pause(); // pysäytetään tausta
+        ullatusPaikka.innerHTML = ""; // tyhjennä ullatusPaikka
+    });
+}
+
+if(kissaNappi) { // tarkistetaan, että kissanappi löytyy
+    kissaNappi.addEventListener('click', function() { // klikatessa sitä suorita funktio
+    musiikki.play(); //soita musiikki
+    haeKisu(); //haeKisu
+    vaihdaIloksi(); //vaihda kuva iloiseksi (aja tämä funktio - se tekee tämän)
+    lisaaStop(); // aja funktio lisaaStop, joka lisää Stop-napin
+    granimInstance.play(); // käynnistetään tausta uusiksi (jotta tausta toimii, vaikka olisi aiemmin klikattu stop)
+});}
+
+// albumitPaikka järjesteltäväksi (jQuery UI)
+$( "#albumitPaikka" ).sortable({
+    revert: true });
+
+// juhlaKissa myös liikuteltavaksi    
+$( "#juhlaKissa" ).sortable({
+    revert: true });
